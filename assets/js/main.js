a1459b6cc5681172d7eadff0107c2b3dd7bf3f80
Henry Majoros <majoros.henry@gmail.com>
2016-06-15 15:29:40 -0400

    guess i should update. compelte pivot. evolves to gengar

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,37 @@
-var menu = document.querySelector('nav'), 
-    menuOpen = document.querySelector('.menu-open'),
-    menuClose = document.querySelector('.menu-close');
+function randomizeBackgrounds() {
+  var posts = document.getElementsByClassName('post-info');
 
-menuOpen.addEventListener('click', openMenu);
-menuClose.addEventListener('click', closeMenu);
+  var offset = Math.floor(Math.random() * 3) + 1;
+  for (var i = 0; i < posts.length; i++) {
 
-function openMenu(event) {
-  event.stopPropagation();
-  menu.classList.remove('offscreen');
-  document.body.addEventListener('click', closeMenu);
+    var num = ((i + offset) % 4) + 1;
+    posts[i].classList.add('bg-' + num);
+  }
 }
 
-function closeMenu() {
-  menu.classList.add('offscreen');
+function replaceImages() {
+  var images = document.querySelectorAll('[data-img]');
+
+  for (var i = 0; i < images.length; i++) {
+    images[i].style.backgroundImage = 'url(' + images[i].dataset.img + ')';
+  }
+}
+
+function colorizePagination() {
+  var num1 = Math.floor(Math.random() * 4) + 1,
+      num2 = (num1 + 1) % 4 + 1;
+  document.querySelector('.prev-post').classList.add('bg-' + num1 + '-hover');
+  document.querySelector('.next-post').classList.add('bg-' + num2 + '-hover');
+}
+
+if (document.body.classList.contains('home-template')) {
+  randomizeBackgrounds();
+  replaceImages();
+}
+
+if (document.body.classList.contains('post-template')) {
+  replaceImages();
+  colorizePagination();
 }
 
 
